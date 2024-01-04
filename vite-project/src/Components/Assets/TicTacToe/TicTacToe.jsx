6b26f8ea5aca2ff9e510825890fc2349b9f1a3f7
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-04 12:24:39 -0500

    basic board styling and formatting complete

--- vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -9,7 +9,21 @@ export const TicTacToe = () => {
     <div className = 'container'>
         <h1 className="title">Tic Tac Toe Game In <span>React.js</span></h1>
         <div className="board">
-
+            <div className="row1">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row2">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row3">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
         </div>
         <button className='reset'>Reset</button>
     </div>
