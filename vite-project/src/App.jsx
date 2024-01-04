f49987296d5489cb0fcab35a498920a3fc5989ef
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-04 11:53:27 -0500

    file structure created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
 import './App.css'
+import { TicTacToe } from './Components/Assets/TicTacToe/TicTacToe'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <TicTacToe/>
     </>
   )
 }
---
+++ vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TicTacToe.css'
+
+export const TicTacToe = () => {
+  return (
+    <div>TicTacToe</div>
+  )
+}
