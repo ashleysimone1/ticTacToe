6f2625843d250a2722f1a28391133109625a5627
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-04 12:16:45 -0500

    basic styling updates

--- vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './TicTacToe.css'
+import circle from './o.png'
+import cross from './x.png'
+
 
 export const TicTacToe = () => {
   return (
-    <div>TicTacToe</div>
+    <div className = 'container'>
+        <h1 className="title">Tic Tac Toe Game In <span>React.js</span></h1>
+        <div className="board">
+
+        </div>
+        <button className='reset'>Reset</button>
+    </div>
   )
 }
