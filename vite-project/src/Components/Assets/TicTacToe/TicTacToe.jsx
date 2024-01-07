c91951b016be75a8ffa54824e5defc2afee1b6b2
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-06 23:14:36 -0500

    images of x and o icons not rendering

--- vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -3,26 +3,48 @@ import './TicTacToe.css'
 import circle from './o.png'
 import cross from './x.png'
 
+let data = ["", "", "", "", "", "", "", "", ""];
 
 export const TicTacToe = () => {
+
+  let [count, setCount] = React.useState(0);
+  let [lock, setLock] = React.useState(false);
+
+  const handlePlayerToggler = (e, num) => {
+    if(lock) {
+      return 0;
+    }
+
+    if(count%2===0){
+      e.target.innerHTML = `<img src=${circle}/>`;
+      data[num] = "cross";
+      setCount(count++);
+    }else{
+      e.target.innerHTML = `<img src=${cross}/>`;
+      data[num] = "circle";
+      setCount(count++);
+    }
+  }
+
+
   return (
     <div className = 'container'>
         <h1 className="title">Tic Tac Toe Game In <span>React.js</span></h1>
         <div className="board">
             <div className="row1">
-                <div className="boxes"></div>
-                <div className="boxes"></div>
-                <div className="boxes"></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 0)}></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 1)}></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 2)}></div>
             </div>
             <div className="row2">
-                <div className="boxes"></div>
-                <div className="boxes"></div>
-                <div className="boxes"></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 3)}></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 4)}></div>
+                <div className="boxes" onClick={(e) =>handlePlayerToggler(e, 5)}></div>
             </div>
             <div className="row3">
-                <div className="boxes"></div>
-                <div className="boxes"></div>
-                <div className="boxes"></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 6)}></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 7)}></div>
+                <div className="boxes" onClick={(e) => handlePlayerToggler(e, 8)}></div>
             </div>
         </div>
         <button className='reset'>Reset</button>
