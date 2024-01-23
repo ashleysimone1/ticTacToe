288adb11f78209cce8cc6dc30f7780e7f298807b
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-23 17:24:25 -0500

    board functionality finalized

--- vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ vite-project/src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -11,35 +11,38 @@ export const TicTacToe = () => {
   let [lock, setLock] = React.useState(false);
 
   const handlePlayerToggler = (e, num) => {
-
+    if(lock) return 0;
     if(count%2===0){
       e.target.innerHTML = `<h1>X</h1>`;
       data[num] = "x";
       setCount(count++);
+      console.log(count); //tried console logging to fix bug and nothing is logging
     }else if(count%2===1){
       e.target.innerHTML = `<h1>O</h1>`;
       data[num] = "o";
       setCount(count++);
+      console.log(count);
     }
+    checkWinner();
   }
 
   const checkWinner = () => {
     if(data[0] === data[1] && data[1] === data[2] && data[0] !== "") {
-      return true;
+      winner(data);
     }else if(data[3] === data[4] && data[4] === data[5] && data[3] !== "") {
-      return true;
+      winner(data);
     }else if(data[6] === data[7] && data[7] === data[8] && data[6] !== "") {
-      return true;
+      winner(data);
     }else if(data[0] === data[3] && data[3] === data[6] && data[0] !== "") {
-      return true;
+      winner(data);
     }else if(data[1] === data[4] && data[4] === data[7] && data[1] !== "") {
-      return true;
+      winner(data);
     }else if(data[2] === data[5] && data[5] === data[8] && data[2] !== "") {
-      return true;
+      winner(data);
     }else if(data[0] === data[4] && data[4] === data[8] && data[0] !== "") {
-      return true;
+      winner(data);
     }else if(data[2] === data[4] && data[4] === data[6] && data[2] !== "") {
-      return true;
+      winner(data);
     }
   }
 
@@ -50,6 +53,7 @@ export const TicTacToe = () => {
   return (
     <div className = 'container'>
         <h1 className="title">Tic Tac Toe Game In <span>React.js</span></h1>
+        <p className="Winner"></p>
         <div className="board">
             <div className="row1">
                 <div className="boxes" onClick={(e) => handlePlayerToggler(e, 0)}></div>
@@ -71,3 +75,4 @@ export const TicTacToe = () => {
     </div>
   )
 }
+ 
